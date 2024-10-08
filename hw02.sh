4e3f80273da39e7d552b553843a7a2f10694249a
matyas <matyasgodula@gmail.com>
2024-10-08 22:01:55 +0200

    works on 2/4

--- hw02.sh
+++ hw02.sh
@@ -3,6 +3,8 @@
 success=0
 failure=1
 
+activated_flag=false
+
 pdf_flag=false
 number_flag=false
 sentence_flag=false
@@ -16,6 +18,11 @@ print_help() {
 }
 
 process_arguments() {
+    if [[ $# -eq 0 ]]; then
+        print_help
+        exit $failure
+    fi
+
     while [[ $# -gt 0 ]]; do
     argument=$1
 
@@ -26,15 +33,24 @@ process_arguments() {
         ;;
         
         -a)
-            pdf_flag=true
+            if [[ $activated_flag == false ]]; then
+                pdf_flag=true
+                activated_flag=true
+            fi
         ;;
 
         -b)
-            number_flag=true
+            if [[ $activated_flag == false ]]; then
+                number_flag=true
+                activated_flag=true
+            fi
         ;;
 
         -c)
-            sentence_flag=true
+            if [[ $activated_flag == false ]]; then
+                sentence_flag=true
+                activated_flag=true
+            fi
         ;;
         *)
             print_help
@@ -47,14 +63,50 @@ done
 }
 
 pdf_listing() {
-    
+    # diables case sensitivity
+    shopt -s nocaseglob
+    for file in *.pdf .*.pdf; do
+        if [ -e "$file" ]; then
+            pdf_files+=("$file")
+        fi
+    done
+    # enables it back
+    shopt -u nocaseglob
+}
+
+stdin_read() {
+    while read -r line; do
+        if [[ $line =~ ^[0-9]+ ]]; then
+            echo "$line" | sed -n 's/^[0-9]\+//p'
+        elif [[ $line =~ ^[+-][0-9]+ ]]; then
+            echo "$line" | sed -n 's/^[+-][0-9]\+//p'
+        fi
+    done
+}
+
+sentences_process() {
+    # reads the input and stores it in text, plus replaces all \n with spaces
+    text=$(tr '\n' ' ')
+    # macthes the criteria for the regex
+    # exaplanation: starts with an uppercase letter, followed by any number of characters that aren .?! and ends with one of them
+    echo "$text" | grep -o -E '[[:upper:]][^.!?]*[.!?]'
 }
 
 # int main(void)
 process_arguments "$@"
 
 if [[ $pdf_flag == true ]]; then
-    echo "pdf flag is true"
+    pdf_files=()
+    pdf_listing
+    printf "%s\n" "${pdf_files[@]}"
+fi
+
+if [[ $number_flag == true ]]; then
+    stdin_read
+fi
+
+if [[ $sentence_flag == true ]]; then
+    sentences_process
 fi
 
 exit $success
